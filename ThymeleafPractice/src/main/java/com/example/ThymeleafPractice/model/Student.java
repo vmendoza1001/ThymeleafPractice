4e7064d247d573aed698aaaa670b352e2e537d64
Vanessa <v.mendoza1@gmail.com>
2023-11-10 23:33:00 -0500

    1. Add checkboxes to student form 2. Add bulleted list to confirmation page

--- ThymeleafPractice/src/main/java/com/example/ThymeleafPractice/model/Student.java
+++ ThymeleafPractice/src/main/java/com/example/ThymeleafPractice/model/Student.java
@@ -1,5 +1,7 @@
 package com.example.ThymeleafPractice.model;
 
+import java.util.List;
+
 public class Student {
 
     private String firstName;
@@ -8,6 +10,7 @@ public class Student {
     private String country;
 
     private String favoriteLanguage;
+    private List<String> favoriteSystems;
 
     public Student() {
     }
@@ -43,5 +46,13 @@ public class Student {
 
     public void setFavoriteLanguage(String favoriteLanguage) {
         this.favoriteLanguage = favoriteLanguage;
+    }
+
+    public List<String> getFavoriteSystems() {
+        return favoriteSystems;
+    }
+
+    public void setFavoriteSystems(List<String> favoriteSystems) {
+        this.favoriteSystems = favoriteSystems;
     }
 }
